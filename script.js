067f2fc5f0b9d6da7d0c2608db08bd721d0cbfad
Martin-J-Larre <martinjlarre@gmail.com>
2022-11-09 23:25:19 -0500

    Render photos with template string is not working

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-const count = 10;
-const apiKey = "";
+const imageContainer = document.getElementById('img-container');
+const loader = document.getElementById('loader');
 
+let photos = [];
+console.log({photos});
+
+const count = 10;
+const apiKey = "PITjojqBtyoChVvLJFRARXtgv1Qef_XwbZQUQ3skWt8";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+const renderPhotos = () => { 
+  photos.forEach( photo => {
+    displayPhoto = `<a href=${photo.links.html} target="_blank">
+                      <img 
+                        src=${photo.urls.regular} 
+                        alt=${photo.alt_description}
+                        title=${photo.alt_description}>
+                    </a>`
+    imageContainer.innerHTML = displayPhoto;
+  });
+}
 
 const getPhotos = async () => { 
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
+    photos = await response.json();
+    console.log("desde promesa", photos);
+    renderPhotos();
+    
   } catch (err) {
     console.log(err);
   }
