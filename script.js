0eb8ec17ff05ccb074c6872cffbf0580b5a59563
Martin-J-Larre <martinjlarre@gmail.com>
2022-12-22 12:06:45 -0500

    Finished

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const imageContainer = document.getElementById('img-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let imagesTotal = 0;
 let photos = [];
 
 const count = 10;
 const apiKey = "PITjojqBtyoChVvLJFRARXtgv1Qef_XwbZQUQ3skWt8";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+const imgLoaded = () => {
+  imagesLoaded++;
+  if (imagesLoaded === imagesTotal) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
 
 const setAttributes = (element, attributes) => { 
   for (const key in attributes) {
@@ -14,6 +25,8 @@ const setAttributes = (element, attributes) => {
 }
 
 const renderPhotos = () => { 
+  imagesLoaded = 0;
+  imagesTotal = photos.length;
   photos.forEach( photo => {
     const a_tag = document.createElement('a');
     setAttributes(a_tag, {
@@ -26,7 +39,9 @@ const renderPhotos = () => {
       src: photo.urls.regular,
       alt: photo.alt_description,
       title: photo.alt_description
-    })
+    });
+
+    img_tag.addEventListener('load', imgLoaded);
 
     a_tag.appendChild(img_tag);
     imageContainer.appendChild(a_tag);
@@ -46,12 +61,9 @@ const getPhotos = async () => {
 }
 
 window.addEventListener('scroll', () => {
-  // console.log("window.scrollY", window.scrollY);
-  // console.log("window.innerHeight", window.innerHeight);
-  // console.log("document.body.offsetHeight", document.body.offsetHeight);
-  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false;
     getPhotos()
-    console.log("load");
   }
 });
 
